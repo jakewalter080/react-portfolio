85d0d9b3f6ee048bce04199591729af15c372257
Jake Walter <jakewalter080@gmail.com>
2025-01-02 19:40:06 -0600

    add footer design

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,3 +1,9 @@
 import React from 'react';
 import { Github, Linkedin, Twitter } from 'lucide-react';
 
+const Footer = () => (
+    <footer className="bg-slate-800 text-white mt-12">
+      <div className="container mx-auto p-4">
+        <div className="flex justify-center space-x-6">
+
+            
